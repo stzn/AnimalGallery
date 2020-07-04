4e4497c0f1b60ae62c9757d5fcdc0ecf5a7b0467
stzn <sztk1209@gmail.com>
2020-07-04 09:59:31 +0900

    Initial Commit

---
+++ DogGallery/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DogGallery
+//
+//  Created by Shinzan Takata on 2020/07/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DogGallery/DogGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGalleryApp.swift
+//  DogGallery
+//
+//  Created by Shinzan Takata on 2020/07/04.
+//
+
+import SwiftUI
+
+@main
+struct DogGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
